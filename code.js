bfa50a16d0a652b6d2affd45639183ad3806fba4
azlo-aj <github@nook.email>
2022-08-18 22:20:59 -0500

    Add tool functionality

--- code.js
+++ code.js
@@ -2,25 +2,37 @@ let gridSize = 64;
 let pixelSize = (550) / gridSize;
 let drawing = false;
 let color = "black";
+let lastTool = "pencil";
 
 
 let row = document.createElement('div');
 let pixel = document.createElement('div');
 let canvas = document.querySelector('#canvas');
 
+function getColor() {
+    if (lastTool === "eraser") {
+        return "white";
+    } else if (lastTool === "rainbow") {
+        return `rgba(${Math.round(Math.random()*255)}, 
+                    ${Math.round(Math.random()*255)}, 
+                    ${Math.round(Math.random()*255, 1)})`;
+    } else {
+        return color;
+    }
+}
 
 function onClick(e) {
-    e.currentTarget.style.backgroundColor = "black";
+    e.currentTarget.style.backgroundColor = getColor();
 
 };
 function onDown(e) {
-    e.currentTarget.style.backgroundColor = "black";
+    e.currentTarget.style.backgroundColor = getColor();
     drawing = true;
 };
 
 function onHover(e) {
     if (drawing === true) {
-        e.currentTarget.style.backgroundColor = "black";
+        e.currentTarget.style.backgroundColor = getColor();
 }};
 function onUp() {
     drawing = false;
@@ -35,14 +47,6 @@ for (i=0; i<gridSize; i++) {
     // row.setAttribute("draggable", false);
     canvas.appendChild(row.cloneNode(true));
 }
-
-// function draw(child, parent) {
-//     varToString = Object.keys({child})[0];
-//     for (i=0; i<gridSize; i++) {
-//         child.className = `${varToString}`;
-//         parent.appendChild(child.cloneNode(true));
-//     }
-// }
 
 allRows = document.querySelectorAll('.row');
 allRows.forEach((node) => {
@@ -64,7 +68,25 @@ document.querySelectorAll('.pixel').forEach( (node) => {
 });
 document.querySelector('html').addEventListener('mouseup', onUp);
 
+document.querySelector('#pencil').addEventListener('click', () => {lastTool = "pencil"});
+document.querySelector('#rainbow').addEventListener('click', () => {lastTool = "rainbow"});
+document.querySelector('#eraser').addEventListener('click', () => {lastTool = "eraser"});
 
-// // row.appendChild(pixel.cloneNode(true));
-// canvas.appendChild(row.cloneNode(true));
-// row.appendChild(pixel.cloneNode(true));
+
+// clock
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    m = checkTime(m);
+    if (h > 12) {
+        h -= 12;
+    }
+    document.getElementById('clock').innerHTML =  h + ":" + m;
+    setTimeout(startTime, 1000);
+  }
+  
+  function checkTime(i) {
+    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+    return i;
+}
